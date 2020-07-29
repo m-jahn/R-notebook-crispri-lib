9833e68cd16a2094dd6783a4405600242e7d7088
Michael Jahn <michael.jahn@scilifelab.se>
2020-07-29 17:37:04 +0200

    added analysis of mixotrophic growth, updated figures

--- Ralstonia-resource-allocation/pipeline/read_rba_result.R
+++ Ralstonia-resource-allocation/pipeline/read_rba_result.R
@@ -21,8 +21,8 @@ read_rba_result <- function(files) {
       into = c("type", "iteration")
     } else {
       simulation = stri_extract_first(new_name,
-        regex = "(succ|fru|for)_[0-9]*(\\.[0-9]*)?_nh4_[0-9]*(\\.[0-9]*)?")
-      into = c("carbon_source", "carbon_conc", "nitrogen_source", "nitrogen_conc")
+        regex = "(succ|fru|for)_[0-9]*(\\.[0-9]*)?_nh4_[0-9]*(\\.[0-9]*)?_[0-9]*")
+      into = c("carbon_source", "carbon_conc", "nitrogen_source", "nitrogen_conc", "sim_run")
     }
     
     # read data
